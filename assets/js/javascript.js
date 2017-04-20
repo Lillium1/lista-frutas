d183e66897ade002c2c2a3154e6c671e0ac1f012
Paulina Gonzalez C <pgc.lacroix@gmail.com>
2017-04-20 20:38:28 -0300

    enumeracion + boton funcionando con alet

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,6 +2,6 @@ var texto = document.getElementById("fruta");
 var boton = document.getElementById("botoncito");
 boton.addEventListener("click", alerta);
 
-function alerta () {
-  alert("Hola Mundo")
+function alerta (){
+	alert("Hola Mundo");
 }
