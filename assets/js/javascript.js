92913ca92bc8d68403d738656ec527264c842861
Paulina Gonzalez C <pgc.lacroix@gmail.com>
2017-04-20 14:35:50 -0300

    arreglos con imagen + fomulario y punteo

---
+++ assets/js/javascript.js
@@ -0,0 +1,7 @@
+var texto = document.getElementById("fruta");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", alerta);
+
+function alerta () {
+  alert("Hola Mundo")
+}
